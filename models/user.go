663c26d4720a910cd9bbd166f42ccdd49aaa8d6f
gauravsahay007 <gauravsahay@Gauravs-MacBook-Air.local>
2026-04-26 03:55:43 +0530

    added important functionalities and changes

--- models/user.go
+++ models/user.go
@@ -1,7 +1,9 @@
 package models
 
 type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"-"`
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Password   string `json:"-"`
+	Email      string `json:"email"`
+	ProfilePic string `json:"profile_pic"`
 }
